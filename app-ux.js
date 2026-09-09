28e72a3ca60547b3fe88d573bcb4640a3add26f7
nolongerzzz <101368523+nolongerzzz@users.noreply.github.com>
2026-09-08 23:56:38 -0700

    HUD corners4

--- app-ux.js
+++ app-ux.js
@@ -30,7 +30,7 @@
         if (typeof exportActiveModel === 'function') exportActiveModel();
       });
     }
-    if (typeof setStatus === 'function') setStatus('HUD corners3');
+    if (typeof setStatus === 'function') setStatus('HUD corners4');
   }
 
   if (document.readyState === 'loading') {
